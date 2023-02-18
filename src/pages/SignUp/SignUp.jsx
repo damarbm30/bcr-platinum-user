644a9dfa3f67072ed28815221e9abc627d69a0f7
yogagayuhmukti <113697219+yogagayuhmukti@users.noreply.github.com>
2023-02-18 10:16:27 +0700

    memperbaiki css

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -17,33 +17,41 @@ const SignUp = () => {
           <h1>Sign Up</h1>
         </div>
         <form className="signup">
-          <label htmlFor="name">Name*</label>
-          <input
-            className="form-control"
-            placeholder="Nama Lengkap"
-            onInput={(e) => {
-              setName(e.target.value);
-            }}
-          />
-          <label htmlFor="name">Email*</label>
-          <input
-            className="form-control"
-            placeholder="Contoh: johndee@gmail.com"
-            onInput={(e) => {
-              setEmail(e.target.value);
-            }}
-          />
-          <label htmlFor="name">Create Password*</label>
-          <input
-            className="form-control"
-            placeholder="6+ karakter"
-            onInput={(e) => {
-              setPassword(e.target.value);
-            }}
-          />
-          <button role="button" type="submit">
-            SignUp
-          </button>
+          <div className="inputform">
+            <label htmlFor="name">Name*</label>
+            <input
+              className="form-control"
+              placeholder="Nama Lengkap"
+              onInput={(e) => {
+                setName(e.target.value);
+              }}
+            />
+          </div>
+          <div className="inputform">
+            <label htmlFor="name">Email*</label>
+            <input
+              className="form-control"
+              placeholder="Contoh: johndee@gmail.com"
+              onInput={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div className="inputform">
+            <label htmlFor="name">Create Password*</label>
+            <input
+              className="form-control"
+              placeholder="6+ karakter"
+              onInput={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div className="inputform">
+            <button role="button" type="submit" className="tombol-signup">
+              SignUp
+            </button>
+          </div>
           <div>
             <h1>{name}</h1>
           </div>
