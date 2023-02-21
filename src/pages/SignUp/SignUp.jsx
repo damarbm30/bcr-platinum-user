f9c56ad844323d63aab2bde19c03a1606ca3db87
yogagayuhmukti <113697219+yogagayuhmukti@users.noreply.github.com>
2023-02-21 21:54:35 +0700

    memperbaiki API, sudah berhasil

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { register, kutak } from "../../assets";
 import { HashLink } from "react-router-hash-link";
-import { useState } from "react";
 import Validation from "./Validation";
+import { isEmpty, get } from "lodash";
+// import { useHistory } from "react-router-dom";
 import "./SignUp.css";
 
 const SignUp = () => {
+  // const history = useHistory();
+  async function daftar() {
+    console.log(values);
+    let result = await fetch(
+      "https://bootcamp-rent-cars.herokuapp.com/customer/auth/register",
+      {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+    result = await result.json();
+    console.warn("result", result);
+    // localStorage.setItem("user-info", JSON.stringify(result));
+    // history.push("/SignUp");
+  }
+
   const [values, setValues] = useState({
     name: "",
     email: "",
@@ -14,12 +35,20 @@ const SignUp = () => {
   const [errors, setErrors] = useState({});
 
   const handleInput = (e) => {
-    setValues({ ...values, [e.target.name]: [e.target.value] });
+    setValues({ ...values, [e.target.name]: e.target.value });
+    console.log(values);
   };
 
   function handleValidation(e) {
     e.preventDefault();
-    setErrors(Validation(values));
+    console.log("masuk");
+    const error = Validation(values);
+    console.log(error);
+    if (isEmpty(error)) {
+      daftar();
+    } else {
+      setErrors(error);
+    }
   }
 
   return (
@@ -38,7 +67,9 @@ const SignUp = () => {
               name="name"
               onChange={handleInput}
             />
-            {errors.name && <p style={{ color: "red" }}>{errors.name}</p>}
+            {get(errors, "name") && (
+              <p style={{ color: "red" }}>{get(errors, "name")}</p>
+            )}
           </div>
           <div className="inputform">
             <label htmlFor="email">Email*</label>
@@ -48,8 +79,9 @@ const SignUp = () => {
               name="email"
               onChange={handleInput}
             />
-            <p>{errors.email}</p>
-            {errors.email && <p style={{ color: "red" }}>{errors.email}</p>}
+            {get(errors, "email") && (
+              <p style={{ color: "red" }}>{get(errors, "email")}</p>
+            )}
           </div>
           <div className="inputform">
             <label htmlFor="password">Create Password*</label>
@@ -59,8 +91,8 @@ const SignUp = () => {
               name="password"
               onChange={handleInput}
             />
-            {errors.password && (
-              <p style={{ color: "red" }}>{errors.password}</p>
+            {get(errors, "password") && (
+              <p style={{ color: "red" }}>{get(errors, "password")}</p>
             )}
           </div>
           <div className="inputform">
