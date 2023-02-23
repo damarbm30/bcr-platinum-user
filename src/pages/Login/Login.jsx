23c58c5860e41a22c82d7c5286ba5467e84458fc
Mohammad Varian <119923811+hellz0ne@users.noreply.github.com>
2023-02-23 21:37:26 +0700

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -8,7 +8,7 @@ import { FormLogin } from "./Verification";
 
 const Login = () => {
     const dispatch = useDispatch();
-    const { isAuthenticated, error } = useSelector((state) => state.auth);
+    // const { isAuthenticated, error } = useSelector((state) => state.auth);
 
     useEffect(() => {
         if (error) {
@@ -45,29 +45,8 @@ const Login = () => {
     };
 return (
     <>
-    <Container className="card-body-form">
-        <Form className="" onSubmit={handleSubmit}>
-            <h3>Log in</h3>
-            <Form.Group className="my-3">
-                <label className="labelLogin">Email</label>
-                <Form.Control type="email" className="form-control" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-            </Form.Group>
-            <Form.Group className="mt-3">
-                <label className="labelLogin">Password</label>
-                <Form.Control type="password" className="form-control" placeholder="Enter password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-            </Form.Group>
-            <p className="sign-up text-right link-login">
-            Don't have an Account? <Link as={Link} to="/register" className="link">Sign Up?</Link>
-            </p>
-            <Button type="submit" className="col-12 border-0 signIn mt-3">
-            Sign in
-            </Button>
-        </Form>
-        <p className="text-center mt-2">
-            <Link as={Link} to="/" className="back-home">Back To Home</Link>
-         </p>
-</Container>
-</>
+    
+    </>
 );
 };
 
