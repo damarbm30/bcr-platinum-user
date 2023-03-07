8a1e35da23fb5b7131d137cac233cae93ad75e68
Damar Buana Murti <damarbm30@gmail.com>
2023-03-07 18:57:12 +0700

    add Sign Up

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import Home from "./Home";
 import Cars from "./Cars";
 import CarDetail from "./CarDetail";
 import Login from "./Login";
+import SignUp from "./SignUp";
 
-
-export { Home, Cars, CarDetail, Login };
+export { Home, Cars, CarDetail, Login, SignUp };
