00bcba0f055fc5cff5a3a8d1f5c19285727af2ea
Mohammad Varian <119923811+hellz0ne@users.noreply.github.com>
2023-03-21 05:22:15 +0700

    update styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,31 +15,29 @@ const Navbar = () => {
   };
   return (
     <nav className="navbar navbar-expand-lg sticky-top py-3 shadow-md-sm">
-      <div className="container-fluid justify-content-between">
-        <div>
-          <Link to="/" className="navbar-brand d-none d-sm-block offset-md-1">
-            <img src={logo} alt="logo" />
-          </Link>
-          <Link
-            to="/"
-            className="navbar-brand offset-md-1 fw-bold d-block d-sm-none"
-          >
-            BCR
-          </Link>
-          <button
-            className="navbar-toggler offset-sm-8 offset-5"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-        </div>
+      <div className="container-fluid justify-content-start-md">
+        <Link to="/" className="navbar-brand d-none d-sm-block offset-md-1">
+          <img src={logo} alt="logo" />
+        </Link>
+        <Link
+          to="/"
+          className="navbar-brand offset-md-1 fw-bold d-block d-sm-none"
+        >
+          BCR
+        </Link>
+        <button
+          className="navbar-toggler offset-sm-8 offset-5"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav justify-content-between">
+          <ul className="navbar-nav offset-lg-8">
             <li className="nav-item">
               <HashLink className="nav-link text-black" to="/#services">
                 Our Services
@@ -66,7 +64,7 @@ const Navbar = () => {
                   className="nav-link btn btn-success text-white"
                   onClick={logOut}
                 >
-                  Hi, {email}
+                  {email}
                 </li>
               </div>
             ) : (
